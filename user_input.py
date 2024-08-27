e21598924bf202432a2743b4c0c51f3cf1b6af31
Esthergithu <ewanjikugithu@gmail.com>
2024-08-27 11:56:17 +0300

    Python Assignment

---
+++ user_input.py
@@ -0,0 +1,14 @@
+name = input ('what is your name?')
+#What is your name? Ivy Chelangat
+print('your name',name)
+age = input ('what is your age?')
+#age 54 years
+print('Your age?',age)
+location = input ('What is your location?')
+ #location Wisconsin city
+print('Your location?',location)
+print("Hello Ivy, you are 54 years old and you live in Wisconsin city.")
+
+
+
+
